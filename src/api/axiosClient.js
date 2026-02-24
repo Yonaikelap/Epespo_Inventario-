16f5603b2a4608cad865635c8c13e856b415c133
Yonaikel-Alonzo <Yonaikel_Alonzo@hotmail.com>
2026-02-24 14:35:41 -0500

    si

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://epespo-inventario.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
